468dc1586ff80e2a58a0c12b2dca52482f33ea0f
mohammed <mohammedsebbagh@gmail.com>
2025-07-03 00:45:40 -0400

    force fresh build

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.tailwindcss.com"></script>
         <link href="/styles.css" rel="stylesheet" />
       </head>
       <body>
